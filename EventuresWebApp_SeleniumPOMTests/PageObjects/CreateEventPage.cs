fd79af9af36ef1eb081eb0a6ebbd6fcf8c60ebb1
Christian Kovachki <christian8251@gmail.com>
2024-04-03 18:48:07 +0300

    Added Create Event Page Object Model

---
+++ EventuresWebApp_SeleniumPOMTests/PageObjects/CreateEventPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventuresWebApp_SeleniumPOMTests.PageObjects
+{
+    public class CreateEventPage : BasePage
+    {
+        private const string CreateEventUrl = BaseUrl + "/Events/Create";
+
+        public CreateEventPage(IWebDriver driver) : base(driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+
+        public bool isCreateEventUrlCorrect()
+        {
+            return isUrlCorrect(CreateEventUrl);
+        }
+    }
+}
