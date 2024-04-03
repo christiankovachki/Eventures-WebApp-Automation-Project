a88761ea0761b8eebfdb5ac8fe5ca6eea1cb8a30
Christian Kovachki <christian8251@gmail.com>
2024-04-03 18:47:21 +0300

    Added new class members

--- EventuresWebApp_SeleniumPOMTests/PageObjects/UserHomePage.cs
+++ EventuresWebApp_SeleniumPOMTests/PageObjects/UserHomePage.cs
@@ -5,11 +5,23 @@ namespace EventuresWebApp_SeleniumPOMTests.PageObjects
 {
     public class UserHomePage : BasePage
     {
+        [FindsBy(How = How.Id, Using = "dropdownMenuLink")]
+        private IWebElement _eventsDropdownMenuLink;
+
         [FindsBy(How = How.CssSelector, Using = ".mt-4 [href='/Events/All']")]
-        private IWebElement _allEventsFromLink;
+        private IWebElement _allEventsLinkFromPage;
+
+        [FindsBy(How = How.CssSelector, Using = ".dropdown-item[href='/Events/All']")]
+        private IWebElement _allEventsLinkFromNav;
 
         [FindsBy(How = How.CssSelector, Using = ".mt-4 [href='/Events/Create']")]
-        private IWebElement _createEventFromLink;
+        private IWebElement _createEventLinkFromPage;
+
+        [FindsBy(How = How.XPath, Using = "//a[contains(text(),'Create Event')]")]
+        private IWebElement _createEventLinkFromNav;
+
+        [FindsBy(How = How.XPath, Using = "//button[contains(text(), 'Logout')]")]
+        private IWebElement _logoutLink;
 
         [FindsBy(How = How.CssSelector, Using = "h1.text-center")]
         private IWebElement _welcomeMessage;
@@ -17,6 +29,42 @@ namespace EventuresWebApp_SeleniumPOMTests.PageObjects
         public UserHomePage(IWebDriver driver) : base(driver)
         {
             PageFactory.InitElements(driver, this);
+        }
+
+        public AllEventsPage ClickAllEventsLinkFromPage()
+        {
+            ClickOnElement(_allEventsLinkFromPage);
+            return new AllEventsPage(driver);
+        }
+
+        public AllEventsPage ClickAllEventsLinkFromNav()
+        {
+            ClickOnElement(_allEventsLinkFromNav);
+            return new AllEventsPage(driver);
+        }
+
+        public CreateEventPage ClickCreateEventLinkFromPage()
+        {
+            ClickOnElement(_createEventLinkFromPage);
+            return new CreateEventPage(driver);
+        }
+
+        public CreateEventPage ClickCreateEventLinkFromNav()
+        {
+            ClickOnElement(_createEventLinkFromNav);
+            return new CreateEventPage(driver);
+        }
+
+        public HomePage ClickLogoutLink()
+        {
+            ClickOnElement(_logoutLink);
+            return new HomePage(driver);
+        }
+
+        public bool isLogoutLinkDisplayed()
+        {
+            WaitForVisibilityOfElement(_logoutLink);
+            return _logoutLink.Displayed;
         }
 
         public string WelcomeMessage { get => _welcomeMessage.Text; }
