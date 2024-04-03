db2ebf5406f76bf5827d91384886ac29001c2eb3
Christian Kovachki <christian8251@gmail.com>
2024-04-03 18:45:21 +0300

    Created All Events Page Model

---
+++ EventuresWebApp_SeleniumPOMTests/PageObjects/AllEventsPage.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace EventuresWebApp_SeleniumPOMTests.PageObjects
+{
+    public class AllEventsPage : BasePage
+    {
+        private const string AllEventsUrl = BaseUrl + "/Events/All";
+
+        [FindsBy(How = How.XPath, Using = "//h1[contains(text(), 'All Events')]")]
+        private IWebElement _pageHeader;
+
+        [FindsBy(How = How.XPath, Using = "//a[contains(text(),'Create New')]")]
+        private IWebElement _createNewLink;
+
+        public AllEventsPage(IWebDriver driver) : base(driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+
+        public bool isAllEventsUrlCorrect()
+        {
+            return isUrlCorrect(AllEventsUrl);
+        }
+
+        public bool isCreateNewLinkDisplayed()
+        {
+            return _createNewLink.Displayed;
+        }
+
+        public string PageHeader { get => _pageHeader.Text; }
+    }
+}
