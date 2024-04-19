c8bca318128ecff3ba77bbd3273c0fe4789b2387
Christian Kovachki <christian8251@gmail.com>
2024-04-20 00:38:28 +0300

    Added functionality for saving screenshots when tests fail

--- EventuresWebApp_SeleniumPOMTests/Tests/BaseTest.cs
+++ EventuresWebApp_SeleniumPOMTests/Tests/BaseTest.cs
@@ -2,13 +2,21 @@
 using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using NUnit.Framework.Interfaces;
 
 namespace EventuresWebApp_SeleniumPOMTests.Tests
 {
     public class BaseTest
     {
+        private string screenshotDir = Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName, "Screenshots");
         protected IWebDriver driver;
         protected HomePage homePage;
+
+        [OneTimeSetUp]
+        public void CleanDir()
+        {
+            CleanDirectory(screenshotDir);
+        }
 
         [SetUp]
         public void SetUp()
@@ -20,10 +28,48 @@ namespace EventuresWebApp_SeleniumPOMTests.Tests
             homePage = new HomePage(driver);
         }
 
-        //[TearDown]
-        //public void TearDown()
-        //{
-        //    driver.Close();
-        //}
+        [TearDown]
+        public void TearDown()
+        {
+            if (TestContext.CurrentContext.Result.Outcome == ResultState.Failure || TestContext.CurrentContext.Result.Outcome == ResultState.Error)
+            {
+                TakeScreenshot();
+            }
+            
+            driver.Close();
+        }
+
+        private void TakeScreenshot()
+        {
+            ITakesScreenshot takesScreenshot = (ITakesScreenshot)driver;
+            var screenshot = takesScreenshot.GetScreenshot();
+            string fileName = $"{TestContext.CurrentContext.Test.Name}_{DateTime.Now:dd-MM-yyyy_HH.mm.ss}.png";
+            string screenshotPath = Path.Combine(screenshotDir, fileName);
+            screenshot.SaveAsFile(screenshotPath);
+        }
+
+        private void CleanDirectory(string directoryPath)
+        {
+            try
+            {
+                if (!Directory.Exists(directoryPath))
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+                else 
+                {
+                    DirectoryInfo directory = new DirectoryInfo(directoryPath);
+
+                    foreach (FileInfo file in directory.GetFiles())
+                    {
+                        file.Delete();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An unexpected error occurred.", ex);
+            }
+        }
     }
 }
